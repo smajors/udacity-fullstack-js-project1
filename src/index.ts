f047ec2b0dd9291425073aa50b9a54e2f2f405c9
Stephen Majors <stephen@majorscreations.com>
2022-04-15 11:35:09 -0500

    Static asset directory updated to use path resolve

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './api/index';
+import path from 'path';
 
 const app = express();
 const port = 3000;
@@ -7,7 +8,7 @@ const port = 3000;
 app.use('/api', routes);
 
 // Define static directories
-app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/assets', express.static(path.resolve(__dirname, '../assets')));
 
 // start the Express server
 app.listen(port, () => {
